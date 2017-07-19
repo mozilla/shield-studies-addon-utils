d18d3deffbc5ad48c0e16de438952a9a919b944e
Marc Robert Wong <marcrobertwong@gmail.com>
2017-07-19 15:02:37 -0700

    Added chooseVariation() with minimal studyConfig.

--- test-addon/bootstrap.js
+++ test-addon/bootstrap.js
@@ -1,6 +1,35 @@
+/* global  __SCRIPT_URI_SPEC__  */
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+// TODO investigate this hackiness
+const { studyUtils } = Cu.import(`${__SCRIPT_URI_SPEC__}/../StudyUtils.jsm`, {});
+
+// TODO move this to a Config.jsm file
+const studyConfig = {
+  studyName: "shieldStudyUtilsTest",
+  "weightedVariations": [
+    {"name": "control",
+      "weight": 1},
+    {"name": "kittens",
+      "weight": 1.5},
+    {"name": "puppers",
+      "weight": 2},  // we want more puppers in our sample
+  ],
+};
+
+async function chooseVariation() {
+  const sample = studyUtils.sample;
+  const clientId = await studyUtils.getTelemetryId();
+  const hashFraction = await sample.hashFraction(studyConfig.studyName + clientId);
+  return sample.chooseWeighted(studyConfig.weightedVariations, hashFraction);
+}
+
 this.install = function(data, reason) {};
 
-this.startup = function(data, reason) {};
+this.startup = async function(data, reason) {
+  const variation = await chooseVariation();
+  console.log(variation);
+};
 
 this.shutdown = function(data, reason) {};
 
