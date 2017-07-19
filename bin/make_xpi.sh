15ca93dcdb3fcaeb18dd06731b2428ec6fa0e79b
Marc Robert Wong <marcrobertwong@gmail.com>
2017-07-19 16:19:38 -0700

    Added make_xpi.sh and build-includes.txt.

---
+++ bin/make_xpi.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BASE_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")""/test-addon"
+TMP_DIR=$(mktemp -d)
+DEST="${TMP_DIR}/test-addon"
+mkdir -p $DEST
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$TMP_DIR"
+}
+trap cleanup EXIT
+
+while read -r LINE || [[ -n "${LINE}" ]]; do
+  mkdir -p "$(dirname "${DEST}/${LINE}")"
+  cp -r "${BASE_DIR}/${LINE}" "$(dirname "${DEST}/${LINE}")"
+done < "${BASE_DIR}/build-includes.txt"
+
+pushd $DEST
+zip -r test-addon.xpi *
+mv test-addon.xpi $BASE_DIR
+popd
