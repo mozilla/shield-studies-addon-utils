beda8c26f665f516480aab7738f3d6ab94ac069c
Fredrik Wollsén <fredrik@neam.se>
2018-04-03 17:15:13 +0300

    Removed webExtensionMsg schema since no longer relevant

--- webExtensionApis/study/src/schemas/index.js
+++ webExtensionApis/study/src/schemas/index.js
@@ -5,6 +5,5 @@ module.exports = {
   "shield-study-addon": require("shield-study-schemas/schemas-client/shield-study-addon.schema.json"), // eslint-disable-line max-len
   "shield-study-error": require("shield-study-schemas/schemas-client/shield-study-error.schema.json"), // eslint-disable-line max-len
   studySetup: require("./schema.studySetup.json"),
-  webExtensionMsg: require("./schema.webExtensionMsg.json"),
   weightedVariations: require("./schema.weightedVariations.json"),
 };
