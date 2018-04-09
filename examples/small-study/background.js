91a83af1bfd7da70fe3fb190159e5dad8177d457
Fredrik Wollsén <fredrik@neam.se>
2018-04-09 17:09:11 +0300

    Npm run format

--- examples/small-study/background.js
+++ examples/small-study/background.js
@@ -1,79 +1,76 @@
-
 // Allow outside override for debug and testing using prefs
 const STUDYPREFS = {
   variationPref: `shield.${addonWidgetId}.testingBranch`,
-  firstrunPref: `shield.${addonWidgetId}.firstrun`
-}
+  firstrunPref: `shield.${addonWidgetId}.firstrun`,
+};
 
 const STUDYCONFIG = {
   // the study
-  weightedVariations: {
-
-  },
+  weightedVariations: {},
 };
 
 // This is a Shield Study template.
 class BaseStudy {
-  async constructor(variation) {
+  constructor(variation) {
     // also sets activeExperiment key
     // makes telmemetry possible
-    await browser.study.configure(variation, ...STUDYCONFIG);
+    browser.study.configure(variation, ...STUDYCONFIG);
   }
 
   async isEligible() {
     let permissions = await browser.study.permissions();
     // could have other reasons to be eligible, such as addons or whatever
-    return permissions.shield
+    return permissions.shield;
   }
 
   // TODO gets reasons
   // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/runtime/onInstalled
-  async installOrDie () {
+  async installOrDie() {
     let eligible = await this.isEligible();
     if (!eligible) {
       // a full ineligible ending TODO
-      browser.study.endStudy('ineligible', ...) ;
+      browser.study.endStudy("ineligible" /*, ...*/);
     }
   }
 
-  async watchExpire () {
+  async watchExpire() {
     // TODO, use the firstRun prefs, timers module etc.
     let becameExpired = false;
     if (becameExpired) {
       // full ending
-      await browser.study.endStudy('expired', ...);
+      await browser.study.endStudy("expired" /*, ...*/);
     }
   }
 
-  watchImportantPrefs () {
+  watchImportantPrefs() {
     // for the important prefs for shield, watch if they change and
     // kill-study if they change
-
     // for list of prefs
     // if (change) browser.study.endStudy('lost-permissions')
   }
 }
 
 (async () => {
+  // 1. variation, it all needs the variation
+  const userVariation =
+    (await browser.study.getPref(variationPref, "string")) ||
+    (await browser.shield.simpleDeterministicVariation(
+      STUDYCONFIG.weightedVariations,
+    ));
 
-// 1. variation, it all needs the variation
-const userVariation = await browser.study.getPref(variationPref,'string') ||
-  await browser.shield.simpleDeterministicVariation(STUDYCONFIG.weightedVariations);
+  // 2. configure.  Now we can send telemetry, because we have a variation
+  const study = new BaseStudy(userVariation);
 
-// 2. configure.  Now we can send telemetry, because we have a variation
-const study = new BaseStudy(userVariation);
+  // 3.  eligible for install?  if not, die.
+  browser.runtime.onInstalled.addListener(study.installOrDie);
 
-// 3.  eligible for install?  if not, die.
-browser.runtime.onInstalled.addListener(study.installOrDie);
+  // 4.  handle install vs. regular run.
+  await browser.shield.attemptRun();
 
-// 4.  handle install vs. regular run.
-await browser.shield.attemptRun();
+  // 5. send a ping
+  await browser.shield.telemetry(aPing);
 
-// 5. send a ping
-await browser.shield.telemetry(aPing);
-
-// 6. force quit the study
-await browser.shield.endStudy(...anEnding, uninstall)
-// endSTudy, ps has scary side effects, describe them.
-
-})()
+  // 6. force quit the study
+  await browser.shield.endStudy(...anEnding, uninstall);
+  // endSTudy, ps has scary side effects, describe them.
+})();
