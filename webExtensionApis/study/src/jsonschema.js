f0740a66169831cc807658ed75052835b8d3d643
Gregg Lind <gregg.lind@gmail.com>
2018-06-05 11:45:55 -0500

    Part 3, the webExtension apis, including schema and (derived) docs/api.md

---
+++ webExtensionApis/study/src/jsonschema.js
@@ -0,0 +1,43 @@
+/* eslint-env commonjs */
+
+ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm");
+
+// eslint-disable-next-line no-undef
+const { ExtensionError } = ExtensionUtils;
+
+const Ajv = require("ajv/dist/ajv.min.js");
+const ajv = new Ajv({
+  // important:  these options make ajv behave like 04, not draft-07
+  schemaId: "auto", // id UNLESS $id is defined. (draft 5)
+  meta: require("ajv/lib/refs/json-schema-draft-04.json"),
+  validateSchema: false,
+});
+
+const jsonschema = {
+  /**
+   * Validates input data based on a specified schema
+   * @param {Object} data - The data to be validated
+   * @param {Object} schema - The schema to validate against
+   * @returns {boolean} - Will return true if the data is valid
+   */
+  validate(data, schema) {
+    const valid = ajv.validate(schema, data);
+    return { valid, errors: ajv.errors || [] };
+  },
+  /**
+   * Validates input data based on a specified schema
+   * @param {Object} data - The data to be validated
+   * @param {Object} schema - The schema to validate against
+   * @throws Will throw an error if the data is not valid
+   * @returns {boolean} - Will return true if the data is valid
+   */
+  validateOrThrow(data, schema) {
+    const valid = ajv.validate(schema, data);
+    if (!valid) {
+      throw new ExtensionError(JSON.stringify(ajv.errors));
+    }
+    return true;
+  },
+};
+
+export default jsonschema;
---
+++ webExtensionApis/study/src/telemetry.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+
+// TODO, making this a seperate file means that we have to pass the error from the other compartment.
+
+/**
+ * Returns array of pings of type `type` in reverse sorted order by timestamp
+ * first element is most recent ping
+ *
+ * searchTelemetryQuery
+ * - type:  string or array of ping types
+ * - n:  positive integer. at most n pings.
+ * - timestamp:  only pings after this timestamp.
+ * - headersOnly: boolean, just the 'headers' for the pings, not the full bodies.
+ *
+ * TODO: Fix shortcoming:
+ * Some pings are sent immediately after one another and it's
+ * original sending order is not reflected by the return of
+ * TelemetryArchive.promiseArchivedPingList
+ * Thus, we can currently only test that the last two pings are the
+ * correct ones but not that their order is correct
+ *
+ *
+ * @param {Object<backstagePass>} TelemetryArchive from TelemetryArchive.jsm
+ * @param {ObjectsearchTelemetryQuery} searchTelemetryQuery See searchSentTelemetry
+ *
+ * @returns {Array} Array of found Telemetry Pings
+ */
+async function searchTelemetryArchive(TelemetryArchive, searchTelemetryQuery) {
+  let { type } = searchTelemetryQuery;
+  const { n, timestamp, headersOnly } = searchTelemetryQuery;
+  // {type, id, timestampCreated}
+  let pings = await TelemetryArchive.promiseArchivedPingList();
+  if (type && !Array.isArray(type)) {
+    type = [type];
+  }
+  if (type) pings = pings.filter(p => type.includes(p.type));
+
+  if (timestamp) pings = pings.filter(p => p.timestampCreated > timestamp);
+
+  if (pings.length === 0) {
+    return Promise.resolve([]);
+  }
+
+  pings.sort((a, b) => b.timestampCreated - a.timestampCreated);
+
+  if (n) pings = pings.slice(0, n);
+  const pingData = headersOnly
+    ? pings
+    : pings.map(ping => TelemetryArchive.promiseArchivedPingById(ping.id));
+
+  return Promise.all(pingData);
+}
+
+// TODO pings report, from the utility addon
+
+module.exports = {
+  searchTelemetryArchive,
+};
